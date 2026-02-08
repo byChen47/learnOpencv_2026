921b6ffa663c02c0e5fbc4025b9b89a0b649ddfa
chenboyao-47 <1574530175@qq.com>
2026-02-08 16:43:41 +0800

    basic element image

--- basicElementImage.py
+++ basicElementImage.py
@@ -1,13 +1,11 @@
 import cv2
 
 img1 = cv2.imread('image.png',1)
-img2 = cv2.imread('image.png',0)
-img3 = cv2.imread('image.png',-1)
+
 
 
 cv2.namedWindow('image1',cv2.WINDOW_NORMAL)
 cv2.imshow('image1',img1)
-cv2.imshow('image2',img2)
-cv2.imshow('image3',img3)
+
 
 cv2.waitKey(0)
